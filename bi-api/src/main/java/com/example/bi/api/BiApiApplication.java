52d5ad2dcf1f8eb30008ec27e9e8691a0a19e21e
Joseph <nguld@itsj-group.com>
2021-03-10 14:45:19 +0700

    add bi-api and purchasing-api

---
+++ bi-api/src/main/java/com/example/bi/api/BiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BiApiApplication.class, args);
+  }
+
+}
---
+++ bi-api/src/test/java/com/example/bi/api/BiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
