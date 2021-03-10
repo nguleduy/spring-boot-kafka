3942e1c41f7874fd03926a24b5d3660d842e4194
Joseph <nguld@itsj-group.com>
2021-03-10 13:33:16 +0700

    clean up

--- user-api/src/main/java/com/example/user/api/api/UserApiController.java
+++ user-api/src/main/java/com/example/user/api/api/UserApiController.java
@@ -2,10 +2,7 @@ package com.example.user.api.api;
 
 import com.example.user.api.model.User;
 import com.example.user.api.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,21 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserApiController implements UserApi {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(UserApiController.class);
-
-  @Autowired
-  private ObjectMapper objectMapper;
-
   @Autowired
   private UserService userService;
 
   @Override
   public ResponseEntity<User> getUser(@ApiParam(value = "", required = true) @PathVariable("id") String id) {
-    return new ResponseEntity<User>(userService.getUser(id), HttpStatus.OK);
+    return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
   }
 
   @Override
   public ResponseEntity<Void> newUser(User billing) {
-    return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
   }
 }
